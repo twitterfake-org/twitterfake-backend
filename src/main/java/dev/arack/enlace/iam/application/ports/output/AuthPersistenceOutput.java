e741da72083e5103e71aef6f285643d8198ceb6f
Arack <arack.rs@gmail.com>
2024-09-06 04:09:31 -0500

    feat: add config azure

--- src/main/java/dev/arack/enlace/iam/application/ports/output/AuthPersistenceOutput.java
+++
@@ -1,7 +0,0 @@
-package dev.arack.enlace.iam.application.ports.output;
-
-import dev.arack.enlace.iam.domain.model.UserEntity;
-
-public interface AuthPersistenceOutput {
-    void register(UserEntity userEntity);
-}
--- src/main/java/dev/arack/enlace/iam/domain/model/RoleEnum.java
+++
@@ -1,6 +0,0 @@
-package dev.arack.enlace.iam.domain.model;
-
-public enum RoleEnum {
-    ADMIN,
-    USER
-}
